537a7bbe57961e025540f75db9b9d6c881af7652
GUA <gua@pku.edu.cn>
2026-08-15 15:12:11 +0800

    add editor snap setup

---
+++ Assets/Editor/SnapInitializer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+[InitializeOnLoad]
+public class SnapInitializer
+{
+    static SnapInitializer()
+    {
+        EditorPrefs.SetFloat("MoveSnapX", 1.2f);
+        EditorPrefs.SetFloat("MoveSnapY", 1f);
+        EditorPrefs.SetFloat("MoveSnapZ", 1.2f);
+    }
+}
